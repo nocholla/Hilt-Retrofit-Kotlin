7eef5efd835218884896bc9ac5995950eb691355
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-12 19:28:36 -0700

    Update GetTasksUseCase.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/domain/usecase/GetTasksUseCase.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/domain/usecase/GetTasksUseCase.kt
@@ -1,4 +1,24 @@
 package com.nocholla.hilt.di.hilt.retrofit.domain.usecase
 
-class GetTasksUseCase {
+import com.nocholla.hilt.di.hilt.retrofit.domain.model.Task
+import com.nocholla.hilt.di.hilt.retrofit.domain.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetTasksUseCase @Inject constructor(private val repository: TaskRepository) {
+    suspend operator fun invoke(): Result<Flow<List<Task>>> {
+        return try {
+            val tasksFlow = repository.getTasks().map { tasks ->
+                if (tasks.isEmpty()) throw Exception("No tasks found")
+                tasks
+            }.catch { e ->
+                throw e
+            }
+            Result.success(tasksFlow)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
