525ccd8c415cae78df0034584c12ad0959bb135f
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-12 18:48:09 -0700

    MyApplication added

---
+++ app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/di/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.nocholla.hilt.di.hilt.retrofit.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    // This class enables Hilt for your application
+}
