579bcb9e95b3ad438815f30276d4a1a890c3bb76
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-12 19:20:32 -0700

    Update TaskRepositoryImpl.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/data/repository/TaskRepositoryImpl.kt
@@ -1,4 +1,35 @@
 package com.nocholla.hilt.di.hilt.retrofit.data.repository
 
-class TaskRepositoryImpl {
+import com.nocholla.hilt.di.hilt.retrofit.data.api.TaskApiService
+import com.nocholla.hilt.di.hilt.retrofit.data.model.TaskDto
+import com.nocholla.hilt.di.hilt.retrofit.data.model.toDomain
+import com.nocholla.hilt.di.hilt.retrofit.data.model.toDto
+import com.nocholla.hilt.di.hilt.retrofit.domain.model.Task
+import com.nocholla.hilt.di.hilt.retrofit.domain.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class TaskRepositoryImpl @Inject constructor(
+    private val taskApiService: TaskApiService
+) : TaskRepository {
+
+    suspend fun getTasks(): Flow<List<Task>> = flow {
+        val taskDtos = taskApiService.getTasks()
+        emit(taskDtos.map { it.toDomain() })
+    }
+
+    suspend fun addTask(name: String): Task {
+        val newTaskDto = taskApiService.addTask(TaskDto(id = 0, title = name, isCompleted = false))
+        return newTaskDto.toDomain()
+    }
+
+    suspend fun toggleTaskCompletion(task: Task): Task {
+        val updatedTaskDto = taskApiService.updateTask(task.id, task.toDto())
+        return updatedTaskDto.toDomain()
+    }
+
+    suspend fun removeTask(taskId: Int) {
+        taskApiService.deleteTask(taskId)
+    }
 }
