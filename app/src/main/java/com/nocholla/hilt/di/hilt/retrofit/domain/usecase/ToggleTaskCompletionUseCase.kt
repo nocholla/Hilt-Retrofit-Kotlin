5c609dfcf16da981e267c138bbfe9795af09c583
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-12 19:30:12 -0700

    Update ToggleTaskCompletionUseCase.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/domain/usecase/ToggleTaskCompletionUseCase.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/retrofit/domain/usecase/ToggleTaskCompletionUseCase.kt
@@ -1,4 +1,17 @@
 package com.nocholla.hilt.di.hilt.retrofit.domain.usecase
 
-class ToggleTaskCompletionUseCase {
+import com.nocholla.hilt.di.hilt.retrofit.domain.model.Task
+import com.nocholla.hilt.di.hilt.retrofit.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class ToggleTaskCompletionUseCase @Inject constructor(private val repository: TaskRepository) {
+    suspend operator fun invoke(task: Task): Result<Task> {
+        return try {
+            val updatedTask = task.copy(isCompleted = !task.isCompleted)
+            val result = repository.toggleTaskCompletion(updatedTask)
+            Result.success(result)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
